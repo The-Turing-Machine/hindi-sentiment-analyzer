d931d044d35c0de3253eba77f5fe740e2ceb5e3e
ExC0d3 <sharma.abhinav503@gmail.com>
2016-03-09 02:06:28 +0530

    corrected JSON response

--- Api/routes/sentiment.js
+++ Api/routes/sentiment.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
     request('https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?text='+txt+'&apikey=e1e36193-7f28-4d45-86a7-838ee79431ca',
     function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            
-            res.send(body);
+            body = JSON.parse(body)
+            res.send(body.aggregate);
 
         }
     })
